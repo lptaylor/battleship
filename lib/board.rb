52d1610da5821d5cd403a8069f2c7c40d23fe6a1
vjp888 <rellik888@gmail.com>
2018-10-09 14:42:29 -0600

    Created board class, working on finishing the grid layout

---
+++ lib/board.rb
@@ -0,0 +1,25 @@
+# all board is going to handle is the layout
+#of the hash in the correct order.
+#Primary variable of board is going to be size,
+# secondary is the hash
+
+
+#create hash of grid points
+#how to print as grid?
+
+class Board
+
+attr_reader :size, :hashgrid
+
+  def initialize(size, hashgrid)
+    @size = size
+    @hashgrid = hashgrid
+  end
+
+  def shape
+    #This defined the shape of the grid based on the size
+    #And lays out the hash accordingly
+  end
+
+  def
+end
