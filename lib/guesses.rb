c8505295205963aa78c11877d130a211ffcd69f7
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-09 14:28:50 -0600

    Added ships and guesses with tests

--- lib/guesses.rb
+++ lib/guesses.rb
@@ -1,9 +1,54 @@
 class Guesses
-attr_reader :shot
-  def initialize(shot)
+attr_reader :shots
+  def initialize(turn)
 
-    @shot = []
+    @shots = []
+    @player_shots = player_shots
+    @computer_shots = computer_shots
+  end
+
+  def player_guess
+    p "Please input a guess"
+    @player_shots = gets.chomp
 
   end
 
+
+
+  def computer_guess
+
+
+  end
+
+  def guess_hit?()
+
+
+  end
+
+  loop do
+    p player_shot_board
+    p player_ship_board
+    player_guess
+    player.guess_hit?
+    player.feedback
+    break if computer_ship_board != s
+
+    p computer_shot_board
+    p computer_ship_board
+    computer_guess
+    computer.guess_hit?
+    computer.feedback
+    break if player_ship_board != s
+
+    p "No winner next turn"
+
+    record_shots
+  end
+
+
+  def record_shots
+
+    @shots << [@player_shots, @computer_shots]
+
+  end
 end
---
+++ readme.md
@@ -0,0 +1,19 @@
+#class Battleship
+
+#def initialize
+
+#welcome message...........
+#input = gets.chomp
+
+end
+  #def play(input)
+    if p
+      start GAME class
+    elsif i
+      p instructions
+    elsif q
+    end
+    else
+    p "nonrecognized input"
+    end
+end
