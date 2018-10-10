4094c68bd38c9641c02e410b6cf17f454f3a676c
vjp888 <rellik888@gmail.com>
2018-10-10 11:20:26 -0600

    Created basic working start file can link to other files as needed

---
+++ test/start_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/start'
+
+class StartTest <MiniTest::Test
+
+
+  def test_it_exists
+    start = Start.new
+
+    assert_instance_of Start, start
+  end
+
+
+
+
+
+end
