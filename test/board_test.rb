4aabf812e648354e288fb59aa61df78e7f46ef43
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-10 16:00:29 -0600

    Adds Board Generator

---
+++ test/board_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+
+  def test_board_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_that_you_can_make_a_board
+    board = Board.new
+
+    board.generate_a_board("d",4)
+
+    assert_equal 0, board.generate_a_board("d",4)
+  end
+
+  def test_it_has_spaces
+
+  end
+end
