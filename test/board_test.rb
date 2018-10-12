7326a6526bd5f038aec729a90de3fc0214fe23e6
vjp888 <rellik888@gmail.com>
2018-10-12 17:06:01 -0600

    Board print words adjusted variables and created test for board

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,10 +2,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
 
-  def test_that_you_can_make_a_board
-    board = Board.new
+class BoardTest <MiniTest::Test
 
-    board.generate_a_board("d",4)
+  def test_board_instance_can_be_made
+    board = Board.new(1)
 
-    assert_equal 0, board.generate_a_board("d",4)
+    assert_instance_of Board, board
   end
+
+  def test_board_can_make_board_hash
+    board = Board.new(2)
+
+    board.generate_a_board
+    hash = {"a1"=>"O", "a2"=>"O", "b1"=>"O", "b2"=>"O"}
+
+    assert_equal hash, board.generate_a_board
+  end
+end
